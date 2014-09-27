012cb78385e000bf595e2c57242cfe025767a710
conanjunn <conanjun@126.com>
2014-09-27 13:35:23 +0800

    scrollshow

--- scrollshow/scrollShow.js
+++ scrollshow/scrollShow.js
@@ -1,83 +1,42 @@
 define(function(require, exports, module) {
-	var ClassScrollShow = function(obj, option) {
-		this.obj = obj;
-		option = $.extend({
-			callbackShow: false,
-			callbackHide: false,
-			deep: false
-		}, option || {});
-		this.deep = option.deep;
-		this.callbackSucc = option.callbackShow;
-		this.callbackErr = option.callbackHide;
-		this.bindEvents = null;
-		this.isScrollVis();
-	};
-	ClassScrollShow.prototype.scrollY = function() {
-		return document.body.scrollTop || document.documentElement.scrollTop;
-	};
-	ClassScrollShow.prototype.posWinTop = function(obj) {
-		if (this.obj.getBoundingClientRect()) {
-			var ro = this.obj.getBoundingClientRect();
-			return ro.top + this.scrollY();
-		} else {
-			var iTop = 0;
-			while (this.obj) {
-				iTop += this.obj.offsetTop;
-				this.obj = this.obj.offsetParent;
+	(function($) {
+		$.fn.scrollShow = function(option) {
+			if (!option) {
+				return this.each(function() {
+					$(window).off('scroll', this.scrollShow).off('resize', this.scrollShowResize);
+				});
 			}
-			return iTop;
-		}
-	};
-	ClassScrollShow.prototype.events = function() {
-		var iClient = document.documentElement.clientHeight,
-			ro = this.obj.getBoundingClientRect(),
-			scrollTop = this.scrollY(),
-			offsetT = this.posWinTop(this.obj) + this.obj.offsetHeight,
-			t = ro.top,
-			bll = this.deep ? (t < iClient && scrollTop < offsetT && !this.obj.scrollShow) : (t <= 0 && scrollTop < offsetT && !this.obj.scrollShow),
-			_bll = this.deep ? ((t >= iClient || scrollTop >= offsetT) && this.obj.scrollShow) : ((t < 0 || scrollTop >= offsetT) && this.obj.scrollShow);
-		if (bll) {
-			this.obj.scrollShow = true;
-			this.callbackSucc && this.callbackSucc.call(this.obj);
-		} else if (_bll) {
-			this.obj.scrollShow = false;
-			this.callbackErr && this.callbackErr.call(this.obj);
-		}
-	};
-	ClassScrollShow.prototype.isScrollVis = function() {
-		this.bindEvents = this.events.bind(this);
-		this.bindEvents();
-		window.addEventListener('scroll', this.bindEvents, false);
-		window.addEventListener('resize', this.bindEvents, false);
-	};
-	ClassScrollShow.prototype.removeEvent = function() {
-		window.removeEventListener('scroll', this.bindEvents, false);
-		window.removeEventListener('resize', this.bindEvents, false);
-	};
-	if (window.jQuery) {
-		/*
-			$('#div1').scrollShow({
-				deep:false,
-				callbackShow:function(){},
-				callbackHide:function(){}
-			});
-			取消事件
-			$('#div1').removeScrollShow();
-		*/
-		jQuery.fn.scrollShow = function(option) {
+			var opt = $.extend({
+				offset: 0, // 正数代表多滚动一段距离在触发showCallback，负数相反
+				showCallback: function() {}, // 当元素出现在屏幕上时的callback， this=>当前元素 参数e=> event
+				hideCallback: function() {} // 当元素离开时的callback， this=>当前元素 参数e=> event
+			}, option || {});
+			var $win = $(window);
 			return this.each(function() {
-				this.xy_scrollShow = new ClassScrollShow(this, option);
+				var $t = $(this);
+				var iOffset = $t.offset().top + opt.offset;
+				var iHeight = parseInt($t.height());
+				var isShow = false;
+				var _this = this;
+				var iScroll = 0;
+				this.scrollShow = function(e) {
+					iScroll = $win.scrollTop();
+					if (iScroll >= iOffset - iHeight && iScroll < iOffset + iHeight && !isShow) {
+						isShow = true;
+						opt.showCallback.call(_this, e);
+					} else if ( (iScroll < iOffset - iHeight || iScroll >= iOffset + iHeight) && isShow) {
+						isShow = false;
+						opt.hideCallback.call(_this, e);
+					}
+				};
+				this.scrollShowResize = function(e) {
+					iScroll = $win.scrollTop();
+					iOffset = $t.offset().top + opt.offset;
+					iHeight = parseInt($t.height());
+					_this.scrollShow(e);
+				};
+				$win.on('scroll', this.scrollShow).on('resize', this.scrollShowResize);
 			});
 		};
-		jQuery.fn.removeScrollShow = function() {
-			return this.each(function() {
-				if (this.xy_scrollShow) {
-					this.xy_scrollShow.removeEvent();
-				}
-			});
-		};
-	}
-	return function(obj, option) {
-		return new ClassScrollShow(obj, option);
-	}
+	})(jQuery);
 });
